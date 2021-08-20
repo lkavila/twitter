27991502933326ccf84f9cbc9a49328fd203e7c4
oemanuel <oemanuel@uninorte.edu.co>
2021-08-20 15:50:45 -0500

    pagina de registro

--- src/views/SignUp/index.jsx
+++ src/views/SignUp/index.jsx
@@ -2,14 +2,26 @@ import { Helmet } from 'react-helmet';
 
 const SignUp = () => {
     return (
-        <div>
+        <>
             <Helmet>
-                <title>My first react app</title>
-                <meta name="hola" content="wasaaa bro" />
+                <title>Únete a Twitter hoy mismo.</title>
+                <meta name="description" content="Únete hoy a Twitter. Regístrate Iniciar sesión. Sigue lo que te interesa. Entérate de lo que está hablando la gente. Únete a la conversación." />
             </Helmet>
-            <h3>App3</h3>
-            <div>holis</div><p>wasa</p>
-        </div>
+            <section>
+                <h2>Registrate en twitter</h2>
+                <form>
+                    <label htmlFor="namedInput">Nombre:</label>
+                    <input id="namedInput" type="text" name="name" />
+                    <label htmlFor="emailInput">Correo:</label>
+                    <input id="emailInput" type="email" name="email" />
+                    <label htmlFor="birthdayInput">Fecha de nacimiento:</label>
+                    <input id="birthdayInput" type="date" name="birthday" />
+                    <label htmlFor="passwordInput">Contraseña:</label>
+                    <input id="passwordInput" type="email" name="password" />
+                    <button aria-label="Registrarse en Twitter">Registrarse</button>
+                </form>
+            </section>
+        </>
     );
 }
 
