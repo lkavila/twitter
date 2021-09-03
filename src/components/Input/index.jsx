ed1b00e85eeec42b268433b44dba73dba1e12249
oemanuel <oemanuel@uninorte.edu.co>
2021-09-03 17:50:07 -0500

    input styles

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,18 +1,19 @@
 const Input = (props) => {
-    const { type, id, name, title, label, ...rest } = props;
+    const { type, id, name, title, ...rest } = props;
 
     return (
-        <div>
-            <label for={id}>
-                {label}
+        <div className='space-y-2 '>
+            <label htmlFor={id} className='text-black-light text-base font-normal'>
+                {title}
+            </label>
                 <input
                     type={type}
                     id={id}
                     name={name}
                     placeholder={title}
                     {...rest}
+                    className='w-full h-12 p-5 text-black rounded-md border border-grey focus:outline-none focus:ring-2 focus:ring-blueTwitter focus:border-transparent'
                 />
-            </label>
         </div>
     )
 }
