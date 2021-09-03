874d6b559b1e6dba3c1629d86411d1bb980377e4
oemanuel <oemanuel@uninorte.edu.co>
2021-09-03 15:42:20 -0500

    root page with styles-responsive

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,21 @@
-const Button = ({ title, ...rest }) =>
-    <div>
-        <button {...rest} className="text-white hover:text-red-500 bg-blueTwitter w-full" >
-            {title}
-        </button>
-    </div>
+const Button = (props) => {
+    const { title, primary, secondary, to, white, ...rest } = props
+    let styles;
+    
+    if (white) {
+        styles = "text-blueTwitter bg-white border border-blueTwitter hover:border-grey  hover:text-grey"
+    }else if( secondary){
+        styles = "text-white bg-grey hover:bg-grey-light"
+    }else{
+        styles = "text-white bg-blueTwitter hover:bg-blueTwitter-light"
+    }
+    return (
+
+        <div>
+            <button className={`font-bold w-full h-12 rounded-md ${styles}`}  {...rest}>
+                {title}
+            </button>
+        </div>
+    )
+}
 export default Button;
