5a70150a902d49cc9d3190b2b7e2912e9ebdbb33
oemanuel <oemanuel@uninorte.edu.co>
2021-09-02 21:48:34 -0500

    firts styles in root 2/09/21 9:40 pm

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,6 +1,6 @@
 const Button = ({ title, ...rest }) =>
     <div>
-        <button {...rest}>
+        <button {...rest} className="text-white hover:text-red-500 bg-blueTwitter w-full" >
             {title}
         </button>
     </div>
