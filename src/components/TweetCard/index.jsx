f97ccb19261c55b3ac36512937f66181bd993157
lkavila <lkavila@uninorte.edu.co>
2021-09-04 11:33:53 -0500

    add styles for tweet component lkavila 4/09/2021 11:33 am

--- src/components/TweetCard/index.jsx
+++ src/components/TweetCard/index.jsx
@@ -1,26 +1,41 @@
 import { Link } from 'react-router-dom';
+
+import TweetButton from './TweetButton'
 import { Avatar } from '../'
 const SmallTweet = () => {
     return (
-        <article>
-            <Link style={{ textDecoration: 'none' }} to="/username/tweet">
+        <div className="container mx-auto flex flex-row border border-grey-light border-opacity-25 md:py-2 max-w-screen-sm">
+            <div className="lg:px-4">
+                <Avatar />
+            </div>
+
+
+            <div>
+
                 <div name="tweet">
                     <div name="info-usuario">
-                        <Avatar />
+
                         <p> <b>Nombre completo</b> @username <span>.</span> fecha tweet</p>
                     </div>
-                    <div name="contenido-tweet">
-                        <p>Que pasa chavales!! Todo bien todo correcto?</p>
-                        <div name="botones">
-                            <button>Reply</button>
-                            <button>Retweet</button>
-                            <button>Like/Unlike</button>
-                            <button>Share</button>
+                    <div name="contenido-tweet" className="max-w-screen-sm">
+                        <Link style={{ textDecoration: 'none' }} to="/username/tweet">
+                            <article>Que pasa chavales!! Todo bien todo correcto? sjkfhjasb fdskjf sjkdh fjsdb gjds gjdshg jksdhkgh sdkhg jkdfh jkghdfkj hgdfjkh gkdjfh kgjhdfjk ghdkjfh</article>
+                        </Link>
+                        {(0 > 5) ? <img alt="media" /> : <div></div>}
+                        <div name="botones" className="container mx-auto flex flex-row justify-between md:py-2.5">
+                            <TweetButton reply hoverColor="#1DA1F2" title="Reply" num={34} style={{ transform: 'rotateY(180deg)' }} />
+                            <TweetButton retweet hoverColor="rgb(0, 186, 124)" title="Retweet" num={34} style={{ transform: 'rotate(90deg)' }} />
+                            <TweetButton like hoverColor="rgb(249, 24, 128)" title="Like" num={34} />
+                            <TweetButton share hoverColor="#1DA1F2" title="Share" />
+                            <div></div>
+                            {//<TweetButton unlike style={{ color: 'rgb(249, 24, 128)' }} />
+                            }
                         </div>
                     </div>
                 </div>
-            </Link>
-        </article>
+
+            </div>
+        </div>
     );
 
 }
