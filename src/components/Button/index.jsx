083be6e0a811a08b4906c332524ee2b080d77a3a
oemanuel <oemanuel@uninorte.edu.co>
2021-09-04 16:23:28 -0500

    navigation bar desktop

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,18 +1,18 @@
 const Button = (props) => {
-    const { title, primary, secondary, to, white, ...rest } = props
+    const { title, primary, secondary, to, white, className, ...rest } = props
     let styles;
-    
+
     if (white) {
         styles = "text-blueTwitter bg-white border border-blueTwitter hover:border-grey  hover:text-grey"
-    }else if( secondary){
+    } else if (secondary) {
         styles = "text-white bg-grey hover:bg-grey-light"
-    }else{
+    } else {
         styles = "text-white bg-blueTwitter hover:bg-blueTwitter-light"
     }
     return (
 
         <div>
-            <button className={`font-bold w-full h-12 rounded-md ${styles}`}  {...rest}>
+            <button className={`font-bold w-full h-12 rounded-md ${styles} ${className}`}  {...rest}>
                 {title}
             </button>
         </div>
