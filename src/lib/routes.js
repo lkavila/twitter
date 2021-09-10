0c1ee1ecac5f912f9390e61128842847275dbfaf
lkavila <lkavila@uninorte.edu.co>
2021-09-10 13:24:20 -0500

    add /:usename route

--- src/lib/routes.js
+++ src/lib/routes.js
@@ -12,17 +12,35 @@ import Notifications from "../views/Notifications";
 import Lists from "../views/Lists";
 
 const routes = {
+    unrestricted: [
+        {
+            title: "Home",
+            path: "/",
+            component: Root,
+        },
+        {
+            title: "Login",
+            path: "/login",
+            component: Login,
+        },
+        {
+            title: "Sign Up",
+            path: "/signup",
+            component: SignUp,
+        },
+        {
+            title: "Password reset: search user",
+            path: "/recover-password",
+            component: RecoverPassword,
+        },
+    ],
     restricted: [
         {
             title: "Home",
             path: "/home",
             component: Home,
         },
-        {
-            title: "Profile",
-            path: "/username",
-            component: Profile,
-        },
+
         {
             title: "Tweet page",
             path: "/username/tweet/:id",
@@ -53,27 +71,10 @@ const routes = {
             path: "/bookmarks",
             component: Bookmarks,
         },
-    ],
-    unrestricted: [
         {
-            title: "Home",
-            path: "/",
-            component: Root,
-        },
-        {
-            title: "Login",
-            path: "/login",
-            component: Login,
-        },
-        {
-            title: "Sign Up",
-            path: "/signup",
-            component: SignUp,
-        },
-        {
-            title: "Password reset: search user",
-            path: "/recover-password",
-            component: RecoverPassword,
+            title: "Profile",
+            path: "/:username",
+            component: Profile,
         },
     ]
 }
