96784c38725d316a9cf2978d23a5db22c4f5fec6
lkavila <lkavila@uninorte.edu.co>
2021-10-04 10:10:49 -0500

    implement signup and login

--- src/components/Nav/NavButton/index.jsx
+++ src/components/Nav/NavButton/index.jsx
@@ -1,20 +1,34 @@
+import { useContext } from 'react';
 import { MdMoreHoriz } from 'react-icons/md'
 import { Avatar } from '../../'
-const NavButton = () =>
-    <button className='min-w-max flex flex-row items-center justify-between p-3 rounded-full hover:bg-grey-light' >
-        <Avatar size='10' />
-        <div className='xl:flex flex-col items-start hidden'>
-            <span className='text-sm font-bold'>
-                Nombre completo
-            </span>
-            <span>
-                @username
-            </span>
+import { AuthContext } from "../../../context/AuthContext";
+import { useHistory } from "react-router";
 
-        </div>
-        <div className=''>
-            <MdMoreHoriz size='20' className="justify-end xl:inline-flex hidden" />
-        </div>
-    </button>
+const NavButton = () => {
+    const { user, logout } = useContext(AuthContext);
+    const history = useHistory();
+    const handleLogOut = () => {
+        logout();
+        history.push("/");
+    }
+    return (
+
+        <button className='min-w-max flex flex-row items-center justify-between p-3 rounded-full hover:bg-grey-light' onClick={handleLogOut}>
+            <Avatar size='10' />
+            <div className='xl:flex flex-col items-start hidden'>
+                <span className='text-sm font-bold'>
+                    {user.name}
+                </span>
+                <span>
+                    @{user.username}
+                </span>
+
+            </div>
+            <div className=''>
+                <MdMoreHoriz size='20' className="justify-end xl:inline-flex hidden" />
+            </div>
+        </button>
+    )
+}
 
 export default NavButton;
--- src/views/Auth/SignUp/index.jsx
+++ src/views/Auth/SignUp/index.jsx
@@ -1,7 +1,33 @@
 import { Link } from 'react-router-dom'
 import Metadata from "../../../lib/metadata";
 import { Input, Form, Button, Logo } from '../../../components'
+import { useState } from 'react';
+import { useHistory } from 'react-router'
+import { signUpUser } from '../../../services/userService'
+
 const SignUp = () => {
+    const [name, setName] = useState("");
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const history = useHistory();
+
+    const handleSignUp = (event) => {
+        event.preventDefault();
+        signUpUser(name, email, username, password, password)
+            .then(data => {
+                if (data.message === "ok") {
+                    const user = data.data;
+                    console.log(user)
+                    history.push("/login");
+                }
+            })
+            .catch((err) => {
+                console.log("err", err);
+            });
+    };
+
     return (
         <>
             <Metadata title="Join Twitter today." description="Join Twitter today. Sign up Sign in. Follow what interests you. Find out what people are talking about. Join the conversation." route="register" />
@@ -11,13 +37,13 @@ const SignUp = () => {
                     <p className='text-black-light text-base font-normal'>My Twitter</p>
                     <h1 className='text-black font-bold text-3xl'>Create your account</h1>
                     <Form>
-                        <Input id="namedInput" type="text" name="name" title='Name' required />
-                        <Input id="userNamedInput" type="text" name="userName" title='Username' required />
-                        <Input id="emailInput" type="email" name="email" title='Email' required />
-                        <Input id="passwordInput" type='password' name="password" title='Password' required />
+                        <Input id="namedInput" type="text" name="name" title='Name' required onChange={event => setName(event.target.value)} value={name} />
+                        <Input id="userNamedInput" type="text" name="userName" title='Username' required onChange={event => setUsername(event.target.value)} value={username} />
+                        <Input id="emailInput" type="email" name="email" title='Email' required onChange={event => setEmail(event.target.value)} value={email} />
+                        <Input id="passwordInput" type='password' name="password" title='Password' required onChange={event => setPassword(event.target.value)} value={password} />
                         <Input id="passwordConfirmatioInput" type='password' name="passwordConfirmation" title='Password Confirmation' required />
                         <div className='pt-8'>
-                            <Button to={'/login'} aria-label='sign up on twitter' title='Sign up' />
+                            <Button onClick={handleSignUp} aria-label='sign up on twitter' title='Sign up' />
                         </div>
                     </Form>
                     <div className='flex justify-center' >
